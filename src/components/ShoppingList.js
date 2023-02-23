6f1e02b9320e99cf4bf9d2099450dc16a0cdcf97
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-02-24 01:28:01 +0300

    Completed Assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -11,17 +11,36 @@ function ShoppingList() {
     setSelectedCategory(category);
   }
   
-  // Add useEffect hook
-  // Update state by passing the array of items to setItems 
+  // Add useEffect hook // Update state by passing the array of items to setItems 
   useEffect(() => {
     fetch("http://localhost:4000/items")
       .then((r) => r.json())
       .then((items) => setItems(items));
   }, []);
 
+ // add this callback function
+  function handleUpdateItem(updatedItem) {
+    const updatedItems = items.map((item) => {
+      if (item.id === updatedItem.id) {
+        return updatedItem;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+     //console.log("In ShoppingCart:", updatedItem);
+   }
+
+// add this callback function
+function handleDeleteItem(deletedItem) {
+  const updatedItems = items.filter((item) => item.id !== deletedItem.id);
+  setItems(updatedItems);
+  //console.log("In ShoppingCart:", deletedItem);
+}
+
 // add this function!
-function handleAddItem(newItem) {
-  setItems([...items, newItem]);
+ function handleAddItem(newItem) {
+   setItems([...items, newItem]);
 }
 
   const itemsToDisplay = items.filter((item) => {
@@ -38,8 +57,14 @@ function handleAddItem(newItem) {
         onCategoryChange={handleCategoryChange}
       />
       <ul className="Items">
+        {/* pass it as a prop to Item */}
         {itemsToDisplay.map((item) => (
-          <Item key={item.id} item={item} />
+          <Item 
+          key={item.id} 
+          item={item} 
+          onUpdateItem={handleUpdateItem}
+          onDeleteItem={handleDeleteItem}
+          />
         ))}
       </ul>
     </div>
